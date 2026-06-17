8d56ef4adc177f7cfc4e7fde9927903e3c7d6752
erik <erikbezerra80@gmail.com>
2026-06-17 10:54:50 -0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import './pages/home.dart';
+void main() {
+  runApp(MaterialApp(home: App()));
+}
+
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int SelectedIndex = 0;
+  final pages = [
+    HomePage(),
+    HomePage()
+    ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:pages[SelectedIndex],
+
+      appBar: AppBar(title: Text('Aplicativo de músico')),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: SelectedIndex,
+        
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home2'),
+        ],
+        
+        onTap: (index){
+          setState(() {
+            SelectedIndex = index;
+          });
+        },
+      ),
+
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  @override
+  State<HomePage> createState() => _HomeState();
+}
+
+class _HomeState extends State<HomePage>{
+  @override
+  Widget build (BuildContext context){
+    return Text('coiso coiso 1');
+  }
+}
