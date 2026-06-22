9cc41b6ed3a1b6aaad99aaa1ec684d0ee202d97f
matheuss . <Matheus>
2026-06-22 10:27:17 -0300

    a

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:projeto/widgets/music_container.dart';
-import 'package:projeto/db/music_dao.dart';
-import 'package:projeto/domain/music.dart';
+import '../db/music_dao.dart';
+import '../domain/music.dart';
 
 MusicsDao music_dao = MusicsDao();
 
 class HomePage extends StatefulWidget{
-  const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomeState();
 }
 
 class _HomeState extends State<HomePage>{
-  List<Music> continuar_ouvindo = [];
   List<Music> musicas_mais_populares = [];
-
+  List<Music> continuar_ouvindo = [];
+  
   @override
   void initState() {
     super.initState();
@@ -23,9 +21,9 @@ class _HomeState extends State<HomePage>{
   }
 
   void loadData() async{
-    print('Loading data!');
     musicas_mais_populares = await MusicsDao().getMaisReproduzidas();
     continuar_ouvindo = await MusicsDao().getHistorico();
+
     await Future.delayed(Duration(seconds: 3));
     setState(() {});
   }
@@ -36,23 +34,23 @@ class _HomeState extends State<HomePage>{
       children: [
         SizedBox(
           height: 200,
-          child: listView_musicas(continuar_ouvindo),
+          child: listView_music(musicas_mais_populares),
         ),
-        SizedBox(height: 20,),
         SizedBox(
           height: 200,
-          child: listView_musicas(musicas_mais_populares),
+          child: listView_music(continuar_ouvindo),
         )
       ],
+
+      
       );
   }
 }
 
-Widget listView_musicas(List<Music> musics){
+Widget listView_music(List<Music> musics){
   return ListView.builder(
     itemCount: musics.length,
     itemBuilder: (conxtet,i){
-      print(i);
       return MusicContainer(music: musics[i]);
     },
   );
